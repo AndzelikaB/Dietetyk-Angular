5e89ea5814d0be72a27f5d99ef2cafef7d204a71
Andzelika Botwina <Andzelika.botwina@gmail.com>
2023-02-26 18:23:26 +0100

    code matching

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ContactComponent } from './pages/inner-pages/contact/contact.component';
+import { OfferComponent } from './pages/inner-pages/offer/offer.component';
+import { PriceListComponent } from './pages/inner-pages/price-list/price-list.component';
+import { VisitComponent } from './pages/inner-pages/visit/visit.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'visit', component: VisitComponent },
+  { path: 'offer', component: OfferComponent },
+  { path: 'price-list', component: PriceListComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ContactComponent } from './pages/inner-pages/contact/contact.component';
+import { OfferComponent } from './pages/inner-pages/offer/offer.component';
+import { PriceListComponent } from './pages/inner-pages/price-list/price-list.component';
+import { VisitComponent } from './pages/inner-pages/visit/visit.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    FooterComponent,
+    HeaderComponent,
+    ContactComponent,
+    OfferComponent,
+    PriceListComponent,
+    VisitComponent,
+    PageNotFoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
